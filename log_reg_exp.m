3764642f81fe3f098c1e83481b2895997b642037
Ben Frey <spartanhooah@gmail.com>
2016-03-28 19:59:19 -0400

    Removed line with few training subsets.

--- log_reg_exp.m
+++ log_reg_exp.m
@@ -8,7 +8,7 @@ train_labels = labels(1:2000);
 test_data = data(2001:4601, :);
 test_labels = labels(2001:4601);
 amounts = [200, 500, 800, 1000, 1500, 2000];
-%amounts = [1000, 1500, 2000];
+
  for amount= amounts
      weights = logistic_train(train_data(1:amount, :), labels(1:amount));
      predictions = test_data * weights;
