c52b537017d408336aa0ad4abe7cf5297856097d
Ben Frey <spartanhooah@gmail.com>
2016-03-28 20:36:27 -0400

    Changed "errors" to "accuracies" in log_reg_exp, added initial plotting.
    Added logistic_l1_train function with skeleton.

--- log_reg_exp.m
+++ log_reg_exp.m
@@ -8,11 +8,15 @@ train_labels = labels(1:2000);
 test_data = data(2001:4601, :);
 test_labels = labels(2001:4601);
 amounts = [200, 500, 800, 1000, 1500, 2000];
+accuracies = [];
 
- for amount= amounts
-     weights = logistic_train(train_data(1:amount, :), labels(1:amount));
-     predictions = test_data * weights;
-     errors = sum(abs(test_labels - predictions));
-     test_error = errors / size(test_labels, 1);
-     disp(test_error);
- end
+for amount = amounts
+    weights = logistic_train(train_data(1:amount, :), labels(1:amount));
+    predictions = test_data * weights;
+    errors = sum(abs(test_labels - predictions));
+    test_error = errors / size(test_labels, 1);
+    accuracies = [accuracies, test_error];
+end
+
+plot(amounts, accuracies)
+axis([200, 2000, 0.98, 1])
