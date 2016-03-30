1f4870f96136d321c84b922fd0622349f8d2d10f
Ben Frey <spartanhooah@gmail.com>
2016-03-30 19:31:26 -0400

    Finalized computation part of sparse_exp.m.

--- sparse_exp.m
+++ sparse_exp.m
@@ -3,7 +3,7 @@ load('ad_data.mat');
 
 % Add bias
 X_train = [ones(size(X_train, 1), 1) X_train];
-y_train = [ones(size(y_train, 1), 1) y_train];
+X_test = [ones(size(X_test, 1), 1) X_test];
 
 pars = [0.01, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1];
 num_selected = zeros(size(pars));
@@ -12,7 +12,7 @@ i = 1;
 for par = pars
     [weights, bias] = logistic_l1_train(X_train, y_train, par);
     num_selected(i) = nnz(weights);
-    predictions = x_test * weights;
+    predictions = X_test * weights;
     [X, Y, T, AUC] = perfcurve(y_test, predictions, 1);
     AUCs(i) = AUC;
     i = i + 1;
